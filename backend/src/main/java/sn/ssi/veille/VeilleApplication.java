d9e19428833cdb3a408e12f3fd5d6f5a7df6213f
mamadbah <bahmamadoubobosewa@gmail.com>
2026-02-02 14:00:42 +0000

    Initialize backend project structure with Maven, including application properties, main application class, and test cases

---
+++ backend/src/main/java/sn/ssi/veille/VeilleApplication.java
@@ -0,0 +1,13 @@
+package sn.ssi.veille;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeilleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/sn/ssi/veille/VeilleApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.ssi.veille;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
